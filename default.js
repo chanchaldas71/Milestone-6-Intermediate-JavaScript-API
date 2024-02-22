1156f8d636288e5f56af0e53e6ee234c6d12692b
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-02-22 13:05:05 +0600

    first commit

---
+++ default.js
@@ -0,0 +1,29 @@
+// default -->if value is not provided, take this as a default.
+// function add(num1, num2) {
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+//     return result;
+// }
+// const sum = add(5, 7);
+// const sum = add(5, );
+// const sum = add( );
+// const sum = add( ,);
+// const sum = add( ,6);
+// const sum = add(1 ,6);
+
+// function add(num1 = 10, num2=10) {
+//     const result = num1 + num2;
+//     console.log(num1, num2, result);
+//     return result;
+// }
+// const sum = add(5);
+// const sum = add(5,7);
+// const sum = add();
+
+function name (firstName, lastName = 'Gupta') {
+    const fullName = firstName + ' '+ lastName;
+    console.log(firstName, lastName, fullName);
+    return fullName;
+}
+const myName = name('abhi', 'poja');
+console.log(myName);
