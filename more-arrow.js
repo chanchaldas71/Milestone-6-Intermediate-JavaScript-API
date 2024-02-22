56a94393ec994e56a357c3ab48522e7854d4e804
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-02-23 01:06:59 +0600

    mew

--- more-arrow.js
+++ more-arrow.js
@@ -1,5 +1,6 @@
 const difference = (x, y) => x -y;
 const multiply = (first, second, third) => first * second* third;
+// single paramiter
 const getAge =(person) =>person.age;
 const student = {name: 'Abhijit', age: 23}
 const age = getAge(student);
@@ -8,3 +9,14 @@ console.log(age);
 const getThird = numbers => numbers[2];
 const third = getThird ([1,2,3,5,6,7,8]);
 console.log(third);
+// no parameter
+const doubleIt = num => num* 2;
+const getPI = () =>Math.PI
+console.log(getPI());
+// large arrow function
+const doMath = (x, y , z) => {
+    const sum = x + y + z;
+    const mult = x * y * z;
+    const result = sum + mult;
+    return result;
+}
