27f7e8003628c07421ce22d7221ae7c93cb7e82d
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-02-22 14:41:05 +0600

    amara  ai ta use kore easyly kaj korte pari amara age jamon+ + kortam and new kono variable ba option add korte jan bar hoya jaito . akon amara jaodi  aita use korle ar line break ar jonno extra \n use korte hoi na so amra sovai  use korbo

--- template.js
+++ template.js
@@ -5,5 +5,14 @@ const name = first +' '+ last+ ' '+ greet ;
 console.log(name);
 const a = 10;
 const b = 20;
-const result = 'Your result is' + a + 'and' + b + 'is' + (a +b);
+const result = 'Your result is ' + a + ' and ' + b + ' is ' + (a +b);
 console.log(result);
+const math = `The sum of ${a} and ${b} is ${a + b}`
+console.log(math)
+const email = 'hi john \n' + 
+'cena cena lage \n'+
+'mara mari korba na ki';
+console.log(email);
+const challenge = `john cena 
+cena naki ochena mair dimu prochur`
+console.log(challenge);
