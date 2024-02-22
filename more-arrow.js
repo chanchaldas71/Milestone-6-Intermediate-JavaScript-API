23dd56b346d2e89237c9dd4ca33c4e1ef3813871
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-02-23 00:33:46 +0600

    jeta bujalm je amra akta function a varibale nite pari and kono akta array ar vitore number golo ba object golo amara function diye access korte pari. const variable = arekta variable => variable same as ager ta . [index number];and then onno akta variable a oi 1 line ar function ta call kore akta array thake amra oi array ar man golo niye moja nite pari. aand jodi amra result dakte chai console.log(variable name)

--- more-arrow.js
+++ more-arrow.js
@@ -1,2 +1,10 @@
 const difference = (x, y) => x -y;
 const multiply = (first, second, third) => first * second* third;
+const getAge =(person) =>person.age;
+const student = {name: 'Abhijit', age: 23}
+const age = getAge(student);
+console.log(age);
+
+const getThird = numbers => numbers[2];
+const third = getThird ([1,2,3,5,6,7,8]);
+console.log(third);
