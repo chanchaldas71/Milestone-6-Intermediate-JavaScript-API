86ca8222418f08743fbdba41d87bf7e4bb496615
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-02-24 13:26:06 +0600

    to jeta bujlam je amara jodi speed.js ta use korte chai mane spead function ta use kori to ai jonno amara kono akta variable a math.max use kore kono list ar sov theke boro number ta amra paite pari and jodi array ar thake amra number golo output paite chai to amader oi array ar variable ar console.log kore variable name dite hobe and tokon amra output parbo

--- spreed.js
+++ spreed.js
@@ -1,2 +1,6 @@
-const max  = Math.max(12, 32, 53, 563 ,13, 32)
+const max  = Math.max(12, 32, 53, 563 ,13, 32);
 console.log(max)
+const numbers = [3, 5, 3, 1, 5, 7, 22, 32,100];
+const arrayMax = Math.max(numbers)
+console.log(...numbers)
+console.log(arrayMax);
